cae991ad33fc4eda098b870696cd7856ef29af5b
JustAtharva <atharvaking2080@gmail.com>
2026-06-19 12:41:23 +0530

    Initial Push

---
+++ src/Operators/Assignment_Operators.java
@@ -0,0 +1,18 @@
+package Operators;
+
+public class Assignment_Operators {
+	public static void main(String[] args) {
+		int a = 10;
+		a += 10;
+		System.out.println(a);
+		
+		a -=5;
+		System.out.println(a);
+		
+		a *=5;
+		System.out.println(a);
+		
+		a /=5;
+		System.out.println(a);
+	}
+}
